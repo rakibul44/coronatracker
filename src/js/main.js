c19fc34dc6f3cd331c050e371697ff22547ec6fa
MD Rakibul Ahsan <MD Rakibul Ahsan>
2021-08-19 16:42:16 +0600

    slide added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
 (function($){
 
-
- 
+  $('.team').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    // navText: ['<i class="fas fa-chevron-right"></i>', '<i class="fas fa-chevron-left"></i>'],
+    margin:160,
+    // nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
        //wow js  
     
        var wow = new WOW(
@@ -21,8 +38,23 @@
       );
       wow.init();
       
+      // top-arrow 
+    $(window).scroll( function(){
+      var dis = $(window).scrollTop();
+      if (dis > 350) {
+          $('.top-arrow').fadeIn();
+          }
+      else{
+          $('.top-arrow').fadeOut();
+      }
+  } );
+  
+  $('.top-arrow').on('click', function(){
+      $('html').animate({ scrollTop : 0 })
+  });
 
-
+// counter js 
+    
 
 }) (jQuery)
     
