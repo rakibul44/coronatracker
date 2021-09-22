57456753732cedb66e893b606211cc01afdeaf7e
Abid Hasan <abidtkg@gmail.com>
2021-09-22 21:11:36 +0600

    Update

--- routes/coronalive.routes.js
+++ routes/coronalive.routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const liveData = require('../database/db.json');
+
+const liveData = {
+    active: 5420014,
+    recoverd: 442524,
+    deths: 457255
+}
 
 router.get('/livetracker', (req, res) => {
     res.status(200).json(liveData);
